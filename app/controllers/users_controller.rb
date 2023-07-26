4e5fc508d7f43f96812ad22cd8bddbdc6bf57ffd
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-07-26 20:37:36 +0000

    0727 05

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,10 +40,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to user_path
-    end
-  end
+  
 end
